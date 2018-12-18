2f3c53e9da1ecf490cfdc875b6c0809c986310ad
Matthew Abrahamson <mra.abrahamson@gmail.com>
2018-12-17 16:54:48 -0800

    basic functionality

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Charity",
+		photo: "https://goo.gl/images/9b5eqy",
+		scores: [5, 1, 2, 3, 1, 2, 5, 5, 1, 1]
+	},
+	{
+		name: "Chastity",
+		photo: "https://goo.gl/images/CDU22Q",
+		scores: [2, 5, 4, 1, 5, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Precious",
+		photo: "https://goo.gl/images/Pdpjbs",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 5, 4]
+	},
+	{
+		name: "Barbee",
+		photo: "https://goo.gl/images/1dwUZh",
+		scores: [3, 1, 3, 5, 2, 3, 5, 2, 5, 5]
+	},
+	{
+		name: "Ginger",
+		photo: "https://goo.gl/images/hD1N9y",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 5, 3]
+	}
+];
+
+module.exports = friends;
