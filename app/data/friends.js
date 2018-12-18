a447c3cbf0aaf34d2c99d76a1ea724035dec2725
Matthew Abrahamson <mra.abrahamson@gmail.com>
2018-12-17 21:02:43 -0800

    pic update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
 		name: "Charity",
-		photo: "https://goo.gl/images/9b5eqy",
+		photo: "https://images.unsplash.com/photo-1518708909080-704599b19972?ixlib=rb-0.3.5&s=0b68ef8d3f182a7d3dc8447f63529c2d&w=1000&q=80",
 		scores: [5, 1, 2, 3, 1, 2, 5, 5, 1, 1]
 	},
 	{
 		name: "Chastity",
-		photo: "https://goo.gl/images/CDU22Q",
+		photo: "https://goo.gl/images/https://www.wpgroups.in/wp-content/uploads/2018/09/9df7d258249699de625e89a574bc152a.jpgCDU22Q",
 		scores: [2, 5, 4, 1, 5, 3, 3, 2, 3, 3]
 	},
 	{
 		name: "Precious",
-		photo: "https://goo.gl/images/Pdpjbs",
+		photo: "https://i.imgur.com/AEQmXx1.jpg",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 5, 4]
 	},
 	{
 		name: "Barbee",
-		photo: "https://goo.gl/images/1dwUZh",
+		photo: "https://images-na.ssl-images-amazon.com/images/I/51MEogKWq3L.jpg",
 		scores: [3, 1, 3, 5, 2, 3, 5, 2, 5, 5]
 	},
 	{
 		name: "Ginger",
-		photo: "https://goo.gl/images/hD1N9y",
+		photo: "http://www.indianlink.com.au/wp-content/uploads/2014/09/generic-indian-girl.jpg",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 5, 3]
 	}
 ];
